06696b4a0fb5015be025db174e59f6fb80a6fbc5
majdeddine fazaa <fazaamajdfazaa@gmail.com>
2022-09-08 19:22:15 +0100

    final commit for resolve

--- lib/app/modules/shifts/views/shift_profile.dart
+++ lib/app/modules/shifts/views/shift_profile.dart
@@ -30,29 +30,6 @@ class ShiftView extends StatefulWidget {
 }
 
 class _ShiftViewState extends State<ShiftView> {
-  //String _address = ""; // create this variable
-//
-  //void getAdresseFromCordination() async {
-  //  List<Placemark> newPlace =
-  //      await placemarkFromCoordinates(widget.latitude, widget.longitude);
-//
-  //  Placemark placeMark = newPlace[0];
-  //  String? name = placeMark.name;
-  //  String? subLocality = placeMark.subLocality;
-  //  String? locality = placeMark.locality;
-  //  String? administrativeArea = placeMark.administrativeArea;
-  //  String? postalCode = placeMark.postalCode;
-  //  String? country = placeMark.country;
-  //  String? address =
-  //      "${name}, ${subLocality}, ${locality}, ${administrativeArea} ${postalCode}, ${country}";
-//
-  //  print(address);
-//
-  //  setState(() {
-  //    _address = address;
-  //  });
-  //}
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
